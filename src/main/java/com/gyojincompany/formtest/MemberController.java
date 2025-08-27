83eacc765b9a014580f0eb83f6ca15b8cb780a98
gyojincompany <luckyred@gmail.com>
2025-08-27 10:53:41 +0900

    매개변수 어노테이션으로 파라미터 값 받아오기 추가

--- src/main/java/com/gyojincompany/formtest/MemberController.java
+++ src/main/java/com/gyojincompany/formtest/MemberController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -16,11 +17,32 @@ public class MemberController {
 		return "login";
 	}
 	
+	@RequestMapping(value = "/login2")
+	public String login2(HttpServletRequest request, Model model) {
+		return "login2";
+	}
+	
 	@RequestMapping(value = "/loginOk")
 	public String loginOk(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response) {
 		
 		String mid = request.getParameter("mid"); //클라이언트가 입력한 아이디값 불러오기
 		String mpw = request.getParameter("mpw"); //클라이언트가 입력한 비밀번호값 불러오기
+		model.addAttribute("mid", mid);
+		if(mid.equals("tiger") && mpw.equals("12345")) { //참이면 로그인 성공
+			session.setAttribute("sessionId", mid); //세션에 로그인한 아이디 올리기		
+			return "loginOk";
+		} else { //로그인 실패
+		
+			return "loginFail";
+		}
+	}
+	
+	@RequestMapping(value = "/loginOk2")
+	public String loginOk2(@RequestParam("mid") String mid, @RequestParam("mpw") String mpw,  Model model, HttpSession session, HttpServletResponse response) {
+		
+//		String mid = request.getParameter("mid"); //클라이언트가 입력한 아이디값 불러오기
+//		String mpw = request.getParameter("mpw"); //클라이언트가 입력한 비밀번호값 불러오기
+		
 		model.addAttribute("mid", mid);
 		if(mid.equals("tiger") && mpw.equals("12345")) { //참이면 로그인 성공
 			session.setAttribute("sessionId", mid); //세션에 로그인한 아이디 올리기		
