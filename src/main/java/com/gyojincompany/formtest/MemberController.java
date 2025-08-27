228d1aaabd5870498b82f2111eec8dc33713aecb
gyojincompany <luckyred@gmail.com>
2025-08-27 10:40:22 +0900

    login form 입력값 가져오기 추가

---
+++ src/main/java/com/gyojincompany/formtest/MemberController.java
@@ -0,0 +1,34 @@
+package com.gyojincompany.formtest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping(value = "/login")
+	public String login(HttpServletRequest request, Model model) {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/loginOk")
+	public String loginOk(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response) {
+		
+		String mid = request.getParameter("mid"); //클라이언트가 입력한 아이디값 불러오기
+		String mpw = request.getParameter("mpw"); //클라이언트가 입력한 비밀번호값 불러오기
+		model.addAttribute("mid", mid);
+		if(mid.equals("tiger") && mpw.equals("12345")) { //참이면 로그인 성공
+			session.setAttribute("sessionId", mid); //세션에 로그인한 아이디 올리기		
+			return "loginOk";
+		} else { //로그인 실패
+		
+			return "loginFail";
+		}
+	}
+
+}
